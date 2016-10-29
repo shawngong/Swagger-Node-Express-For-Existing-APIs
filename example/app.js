44d3220343dae7e21563f80001dfe6c7d26bcb7b
Shawn Chen Gong <sc2gong@uwaterloo.ca>
2016-10-29 11:12:01 -0700

    setting up previous tutorial config

--- example/app.js
+++ example/app.js
@@ -4,3 +4,53 @@ var express = require( 'express' );
 var argv = require('minimist')(process.argv.slice(2));
 var swagger = require("swagger-node-express");
 var bodyParser = require( 'body-parser' );
+
+var app = express();
+var subpath = express();
+
+app.use(bodyParser());
+app.use("/v1", subpath);
+
+swagger.setAppHandler(subpath);
+
+app.use(express.static('dist'));
+
+swagger.setApiInfo({
+        title: "example API",
+        description: "API to do something, manage something...",
+        termsOfServiceUrl: "",
+        contact: "yourname@something.com",
+        license: "",
+        licenseUrl: ""
+});
+
+app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/dist/index.html');
+});
+
+swagger.configureSwaggerPaths('', 'api-docs', '');
+
+// Configure the API domain
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".')
+
+// Configure the API port
+var port = 8080;
+if(argv.port !== undefined)
+    port = argv.port;
+else
+    console.log('No --port=xxx specified, taking default port ' + port + '.')
+
+// Set and display the application URL
+var applicationUrl = 'http://' + domain + ':' + port;
+console.log('snapJob API running on ' + applicationUrl);
+
+
+swagger.configure(applicationUrl, '1.0.0');
+
+
+// Start the web server
+app.listen(port);
